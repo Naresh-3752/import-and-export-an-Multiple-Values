dc7c4e9852f4c9f76ea8ab93b756973309f446dc
root <abramainnaresh4@gmail.com>
2023-09-08 04:12:28 +0000

    import and export an Multiple Values Problem

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,6 @@
+export const myArray = ["camel", 265, true, "5.6"];
+export const bulb = { watts: 10, type: "LED" };
+export function multiplyByFour(text) {
+  return (text =
+    "Should accept a number and return a number multiplied by four");
+}
